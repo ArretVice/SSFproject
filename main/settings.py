1d0428d91983709b271cbec96943b57dea046d1b
arretvice <arretvice@gmail.com>
2019-05-23 19:47:52 +0300

    Turning off DEBUG

--- main/settings.py
+++ main/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SSF_PROJECT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['ssfproject.herokuapp.com', 'localhost']
 
